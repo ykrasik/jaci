c3502c4492288e40cfd3b3351ce616b28b2b0809
ykrasik <ykrasik@gmail.com>
2015-05-28 15:52:55 +0300

    [Restructuring] Utils module

---
+++ jemi-utils/src/main/java/com/github/ykrasik/jemi/util/opt/Absent.java
@@ -0,0 +1,118 @@
+/******************************************************************************
+ * Copyright (C) 2015 Yevgeny Krasik                                          *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ * http://www.apache.org/licenses/LICENSE-2.0                                 *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+package com.github.ykrasik.jemi.util.opt;
+
+import com.github.ykrasik.jemi.util.function.Function;
+import com.github.ykrasik.jemi.util.function.Predicate;
+import lombok.NonNull;
+
+import java.util.*;
+
+/**
+ * @author Yevgeny Krasik
+ */
+// TODO: JavaDoc
+@SuppressWarnings("unchecked")
+final class Absent<T> extends Opt<T> {
+    private static final long serialVersionUID = 0;
+    private static final Absent<?> INSTANCE = new Absent<>();
+
+    private Absent() { }
+
+    @Override
+    public boolean isPresent() {
+        return false;
+    }
+
+    @Override
+    public T get() {
+        throw new IllegalStateException("Called get() on an absent value!");
+    }
+
+    @Override
+    public T getOrElseNull() {
+        return null;
+    }
+
+    @Override
+    public T getOrElse(T defaultValue) {
+        return defaultValue;
+    }
+
+    @Override
+    public Opt<T> orElse(@NonNull Opt<? extends T> alternative) {
+        return (Opt<T>) alternative;
+    }
+
+    @Override
+    public <V> Opt<V> map(Function<? super T, V> function) {
+        return instance();
+    }
+
+    @Override
+    public <V> Opt<V> flatMap(Function<? super T, Opt<V>> function) {
+        return instance();
+    }
+
+    @Override
+    public boolean exists(Predicate<? super T> predicate) {
+        return false;
+    }
+
+    @Override
+    public Opt<T> filter(Predicate<? super T> predicate) {
+        return instance();
+    }
+
+    @Override
+    public List<T> toList() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Set<T> toSet() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public <K> Map<K, T> toMap(K key) {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return object == this;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Opt.absent()";
+    }
+
+    private Object readResolve() {
+        return INSTANCE;
+    }
+
+    static <T> Opt<T> instance() {
+        return (Opt<T>) INSTANCE;
+    }
+}
